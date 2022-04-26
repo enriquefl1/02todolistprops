bb10e2c136878d93438f0b5ee68ad6b9a21c87b8
Enrique Flores <enrique.flores.lopez@hotmail.com>
2022-04-26 00:27:13 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="valorTarea" />
+    <input type="button" v-on:click="agregarTarea" value="Agregar" />
+
+    <ListaTareas :nodoTarea="nodoTarea" @eliminarTarea="eliminarTarea" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ListaTareas from "@/components/ListaTareas.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ListaTareas,
+  },
+  data() {
+    return {
+      message: "Ingrese una tarea en el campo de texto",
+      valorTarea: "",
+      nodoTarea: ["Comprar Pan", "Lavar los platos", "Lavar los vasos"],
+    };
+  },
+  methods: {
+    agregarTarea() {
+      this.valorTarea === ""
+        ? alert("No puedes dejar el campo valor vacio")
+        : this.nodoTarea.push(this.valorTarea);
+      this.valorTarea = "";
+    },
+    eliminarTarea(i) {
+      this.nodoTarea.splice(i, 1);
+    },
   },
 };
 </script>
@@ -24,5 +45,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.lista {
+  width: 290px;
+  margin: 0 auto;
+  text-align: left;
 }
 </style>
---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lista">
+    <ul>
+      <li v-for="(nodo, i) in nodoTarea" :key="i">
+        {{ nodo }}
+        <input type="button" v-on:click="eliminarTarea(i)" value="Eliminar" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListaTareas",
+  props: {
+    nodoTarea: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    eliminarTarea(i) {
+      this.$emit("eliminarTarea", i);
+    },
+  },
+};
+</script>
